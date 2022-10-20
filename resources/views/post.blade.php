96d1676760e65d076210596fadae17a00e1648a9
sunshade88 <jasonsglim@gmail.com>
2022-10-20 14:07:04 +0800

    1st commit - up to ch09

---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1>
+            <?= $post; ?>
+        </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, velit veritatis. Provident cupiditate
+            adipisci aperiam obcaecati commodi quis accusantium sed dolore veniam, unde, corrupti aliquam temporibus!
+            Nulla assumenda sunt facilis!</p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
+
+</html>
