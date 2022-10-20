32f19ae57102b96fd7ea1fcd8ae334fdd15d0159
sunshade88 <jasonsglim@gmail.com>
2022-10-20 14:32:00 +0800

    complete ch10

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,14 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-    
-    // ddd($path);
-    
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         // ddd('file does not exist');
         // abort(404);
         return redirect('/');
     }
-    $post = file_get_contents($path);
+    
+    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+    
     
     return view ('post', [
         'post' => $post
