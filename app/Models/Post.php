415f9abacd64da881800e344bc3282a1e6f373cd
sunshade88 <jasonsglim@gmail.com>
2022-10-20 16:13:21 +0800

    revised ch10 to ch11

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+// class Post extends Model
+// {
+//     use HasFactory;
+// }
+
+class Post
+{
+    public static function all()
+    {
+        $files = File::files(resource_path("posts/"));
+        return array_map(fn($files)=> $files->getContents(), $files);
+    }
+    
+    public static function find($slug)
+    {
+        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+            throw new ModelNotFoundException();
+        }
+
+        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+    }
+
+    
+}
+
+
+
+
+
+
+
+//
