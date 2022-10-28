7d8df66556e2a8d3fd821400917a85c44152309b
sunshade88 <jasonsglim@gmail.com>
2022-10-28 08:27:32 +0800

    ch36 completed

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -23,8 +23,14 @@ class PostFactory extends Factory
             'category_id' => Category::factory(),
             'title' => $this->faker->sentence(),
             'slug' => $this->faker->slug(),
-            'excerpt' => $this->faker->sentence(),
-            'body' => $this->faker->paragraph()
+            'excerpt' => '<p>' . implode('</p><p>', $this->faker->paragraphs(2)) . '</p>',
+            'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(5)) . '</p>'
         ];
     }
 }
+
+//'excerpt' => collect($this->faker->paragraphs(2))->map(fn($item) => "<p>{$item}</p>")->implode(''),
+
+
+
+//
