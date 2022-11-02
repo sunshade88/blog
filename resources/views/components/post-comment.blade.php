349a5be397c1d4f77dcd2e761c3d91afb83d0b4f
sunshade88 <jasonsglim@gmail.com>
2022-11-02 15:46:14 +0800

    ch53 completed

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+<article class="flex bg-gray-100 border border-gray-300 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div class="">
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum est quibusdam autem sunt enim voluptas facilis quisquam natus molestias!
+            </p>
+    </div>
+</article>
