8f0277b18a94884945889177b8984f231b9806dd
sunshade88 <jasonsglim@gmail.com>
2022-11-03 10:18:53 +0800

    ch54 completed

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -36,6 +36,11 @@ class Post extends Model
 
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,20 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-300 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div class="">
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
             <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum est quibusdam autem sunt enim voluptas facilis quisquam natus molestias!
+                {{ $comment->body  }}
             </p>
     </div>
 </article>
