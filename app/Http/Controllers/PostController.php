03330f0faa5df4ee68c9db8972eaf9b290c2c834
sunshade88 <jasonsglim@gmail.com>
2022-11-15 09:43:28 +0800

    ch64 completed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,8 +30,15 @@ class PostController extends Controller
 
     public function store()
     {
+        // dd(request()->file('thumbnail'));
+
+        // $path = request()->file('thumbnail')->store('thumbnails');
+
+        // return 'Done: ' . $path;
+
         $attributes = request()->validate([
             'title' => 'required',
+            'thumbnail'=> 'required|image',
             'slug' => ['required', Rule::unique('posts', 'slug')],
             'excerpt' => 'required',
             'body' => 'required',
@@ -39,6 +46,7 @@ class PostController extends Controller
         ]);
 
         $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
 
         Post::create($attributes);
 
